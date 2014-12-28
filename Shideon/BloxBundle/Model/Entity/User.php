1eeb0eed6292a91700ba8102084f1e551f66deb3
John Pancoast <shideon@gmail.com>
2014-12-28 13:32:24 -0800

    Issue #1 - Added and configured FOS User Bundle

---
+++ Shideon/BloxBundle/Model/Entity/User.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Entity - user
+ *
+ * @copyright (c) 2014-215 John Pancoast
+ * @author John Pancoast <shideon@gmail.com>
+ */
+
+namespace Shideon\BloxBundle\Model\Entity;
+
+use FOS\UserBundle\Model as FOSModel; 
+
+/**
+ * Entity - user
+ *
+ * @author John Pancoast <shideon@gmail.com>
+ */
+class User extends FOSModel\User
+{
+}
