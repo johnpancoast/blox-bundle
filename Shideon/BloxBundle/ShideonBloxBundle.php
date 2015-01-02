2451f5f39b17dc115177fa9f237e6c4822cdc2ee
John Pancoast <shideon@gmail.com>
2015-01-01 20:18:16 -0800

    shideon blox bundle extends fos user bundle (although i'm not happy about needing to extend it... we'll see)

--- Shideon/BloxBundle/ShideonBloxBundle.php
+++ Shideon/BloxBundle/ShideonBloxBundle.php
@@ -6,4 +6,11 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ShideonBloxBundle extends Bundle
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
