ca15147f7c700e203b3770c2eed20d49660b77ae
John Pancoast <shideon@gmail.com>
2015-01-10 22:47:15 -0800

    added user model stub

---
+++ src/Shideon/BloxBundle/Model/UserModel.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package blox
+ * @copyright (c) 2014-2015 John Pancoast
+ * @author John Pancoast <shideon@gmail.com>
+ * @license MIT
+ */
+
+namespace Shideon\BloxBundle\Model;
+
+/**
+ * UserModel
+ *
+ * @package blox
+ * @copyright (c) 2014-2015 John Pancoast
+ * @author John Pancoast <shideon@gmail.com>
+ */
+class UserModel
+{
+    public function test();
+}
